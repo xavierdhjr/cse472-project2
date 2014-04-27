91f8b20a065f850d415aa909235850422586d146
Xavier Durand-Hollis <durandh1@arctic.cse.msu.edu>
2014-04-26 21:17:09 -0400

    Beans

--- ParticleSystem/ParticleEmitter.h
+++ ParticleSystem/ParticleEmitter.h
@@ -1,19 +1,8 @@
 #pragma once
-struct Vector3
-{
-	double x;
-	double y;
-	double z;
-};
 
 struct Particle
 {
-	Vector3 position;
-	Vector3 velocity;
-	int age;
-	int lifespan;
-	int color;
-	int size;
+
 };
 
 class CParticleEmitter
