6d3076b648aff8ef5de4ebc55a8e902602544fc9
Xavier Durand-Hollis <durandh1@arctic.cse.msu.edu>
2014-04-26 21:15:08 -0400

    added particle

--- ParticleSystem/ParticleEmitter.h
+++ ParticleSystem/ParticleEmitter.h
@@ -1,7 +1,19 @@
 #pragma once
+struct Vector3
+{
+	double x;
+	double y;
+	double z;
+};
+
 struct Particle
 {
-
+	Vector3 position;
+	Vector3 velocity;
+	int age;
+	int lifespan;
+	int color;
+	int size;
 };
 
 class CParticleEmitter
