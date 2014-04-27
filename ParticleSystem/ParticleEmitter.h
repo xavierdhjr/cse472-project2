ca5541f313c67251410e418728b6ff114249abfb
Xavier Durand-Hollis <durandh1@arctic.cse.msu.edu>
2014-04-26 22:13:27 -0400

    Added things to struct. added GLEW

--- ParticleSystem/ParticleEmitter.h
+++ ParticleSystem/ParticleEmitter.h
@@ -1,10 +1,30 @@
 #pragma once
 class ParticleComponent;
 #include <vector>
+#include "ShaderWnd\external\glew-1.9.0\include\GL\glew.h"
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include "ShaderWnd\external/glew-1.9.0/include/GL/glext.h"
+#include "ShaderWnd\external/glew-1.9.0/include/GL/wglext.h"
+#include "ShaderWnd\external/glm/glm.hpp"
+#include "ShaderWnd\external/glm/gtc/constants.hpp"
+#include "ShaderWnd\external/glm/gtc/matrix_transform.hpp"
+#include "ShaderWnd\external//glm/gtc/type_ptr.hpp"
+#include <math.h>
+#include "graphics/GrTexture.h"
+
+using namespace glm;
 
 struct Particle
 {
-
+	vec4 color;
+	vec3 position;
+	mat4 rotation;
+	int lifetime;
+	int age;
+	float size;
+	vec3 velocity;
+	CGrTexture* texture;
 };
 
 class CParticleEmitter
